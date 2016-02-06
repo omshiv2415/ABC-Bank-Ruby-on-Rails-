5d280eeb7180736b1e4bf2c89d949594755c5be9
omshiv2415 <omshiv2415@yahoo.co.uk>
2016-02-06 18:08:00 +0000

    account

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,10 @@ class HomeController < ApplicationController
   def index
 
 	   	if current_user.try(:admin?)
-       @accounts = Account.paginate(page: params[:page], per_page: 10)
+				@accounts = Account.order(name: :asc).paginate(page: params[:page], per_page: 10)
 		  #	@accountsByCust = Account.balance.where(:user_id => current_user.id)
 		   elsif current_user
-       @transactions = current_user.transactions.paginate(page: params[:page], per_page: 5)
+       @transactions = current_user.transactions.all.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
 
 	 	end
   end
