0b6dfed61bb4ea7b3c82ad949f852103c05c3cd1
omshiv2415 <omshiv2415@yahoo.co.uk>
2016-02-06 15:15:27 +0000

    present

---
+++ db/migrate/20160206150856_add_column_to_transactions.rb
@@ -0,0 +1,6 @@
+class AddColumnToTransactions < ActiveRecord::Migration
+  def change
+		add_column :transactions, :t_balance, :float
+		add_column :transactions, :total_balance, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204183558) do
+ActiveRecord::Schema.define(version: 20160206150856) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "customer_id"
@@ -27,9 +27,8 @@ ActiveRecord::Schema.define(version: 20160204183558) do
     t.string   "name"
     t.string   "phone"
     t.integer  "user_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.boolean  "verify",      default: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "branches", force: :cascade do |t|
